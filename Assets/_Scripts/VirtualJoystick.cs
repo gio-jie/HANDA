1b8acfeef18722991c5e5a8f528aaf768767bb7b
gio-jie <gioguevan@gmail.com>
2026-01-31 15:04:39 +0800

    Revised Joystick (DONE)

--- Assets/_Scripts/VirtualJoystick.cs
+++ Assets/_Scripts/VirtualJoystick.cs
@@ -4,48 +4,75 @@ using UnityEngine.EventSystems;
 
 public class VirtualJoystick : MonoBehaviour, IDragHandler, IPointerUpHandler, IPointerDownHandler
 {
-    private Image bgImage;
-    private Image handleImage;
+    [Header("Visuals")]
+    public RectTransform joystickBackground; // Yung malaking bilog
+    public RectTransform joystickHandle;     // Yung maliit na bilog
+    
     private Vector2 inputVector; 
+    private Vector2 defaultPos; // Original position
 
     void Start()
     {
-        bgImage = GetComponent<Image>();
-        // Hanapin ang Handle na nasa loob nito
-        handleImage = transform.GetChild(0).GetComponent<Image>(); 
+        // Save original position
+        defaultPos = joystickBackground.anchoredPosition;
+    }
+
+    public void OnPointerDown(PointerEventData ped)
+    {
+        // 1. Hanapin ang World Position ng tap mo
+        Vector3 worldPoint;
+        
+        // Gamitin ang parent ng background (JoystickArea) bilang reference frame
+        if (RectTransformUtility.ScreenPointToWorldPointInRectangle(
+            joystickBackground.parent as RectTransform, 
+            ped.position, 
+            ped.pressEventCamera, 
+            out worldPoint))
+        {
+            // 2. Direktang ilipat ang joystick sa world point na nakuha (Snap to finger)
+            // Note: .position ang gamit natin, HINDI .anchoredPosition
+            joystickBackground.position = worldPoint;
+        }
+
+        // 3. Reset Handle sa gitna
+        joystickHandle.anchoredPosition = Vector2.zero;
+        inputVector = Vector2.zero;
     }
 
     public void OnDrag(PointerEventData ped)
     {
         Vector2 pos;
-        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(bgImage.rectTransform, ped.position, ped.pressEventCamera, out pos))
+        // Calculation kung saan hihilahin ang handle relative sa background
+        if (RectTransformUtility.ScreenPointToLocalPointInRectangle(joystickBackground, ped.position, ped.pressEventCamera, out pos))
         {
-            pos.x = (pos.x / bgImage.rectTransform.sizeDelta.x);
-            pos.y = (pos.y / bgImage.rectTransform.sizeDelta.y);
+            pos.x = (pos.x / joystickBackground.sizeDelta.x);
+            pos.y = (pos.y / joystickBackground.sizeDelta.y);
 
             inputVector = new Vector2(pos.x * 2, pos.y * 2);
             inputVector = (inputVector.magnitude > 1.0f) ? inputVector.normalized : inputVector;
 
             // Galawin ang Handle visual
-            handleImage.rectTransform.anchoredPosition = new Vector2(inputVector.x * (bgImage.rectTransform.sizeDelta.x / 3), inputVector.y * (bgImage.rectTransform.sizeDelta.y / 3));
+            joystickHandle.anchoredPosition = new Vector2(
+                inputVector.x * (joystickBackground.sizeDelta.x / 3), 
+                inputVector.y * (joystickBackground.sizeDelta.y / 3)
+            );
         }
-    }
-
-    public void OnPointerDown(PointerEventData ped)
-    {
-        OnDrag(ped); 
     }
 
     public void OnPointerUp(PointerEventData ped)
     {
+        // Pag binitawan, ibalik sa gilid (default position)
+        joystickBackground.anchoredPosition = defaultPos;
+        
+        // Reset sa gitna
         inputVector = Vector2.zero;
-        handleImage.rectTransform.anchoredPosition = Vector2.zero;
+        joystickHandle.anchoredPosition = Vector2.zero;
     }
 
     public float Horizontal()
     {
         if (inputVector.x != 0) return inputVector.x;
-        else return Input.GetAxis("Horizontal"); // Para gumana rin ang Keyboard arrow keys
+        else return Input.GetAxis("Horizontal");
     }
 
     public float Vertical()
